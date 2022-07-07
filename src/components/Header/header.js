8a26129a2bdbf4e43a799b7e82a8549f70101cb4
Melly5 <92356658+Melly5@users.noreply.github.com>
2022-07-07 19:45:13 +0700

    add button styles
    doesnt work as need

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,14 +2,13 @@ import React, { useState } from 'react';
 import './header.css';
 import logo from '../../assets/logo.png';
 import { Button } from '@mui/material';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-    const [style, setStyle] = useState("but-unclicked");
-  
-    const changeStyle = () => {
-        setStyle("but-clicked");
-    };
+    const [style, setStyle] = useState('');
+    const pathname = useLocation().pathname;
+	const assignStyle = (path) => pathname === path ? "but-clicked" : "but-unclicked";
+
 
     return (
         <div className="header">
@@ -17,11 +16,11 @@ const Header = () => {
                 <img src={logo} alt="logo"></img>
             </div>
             <div className='header-sign'>
-                <Button className={style} onClick={changeStyle}>
-                    <Link to="/login" style={{ textDecoration: 'none' }}>Sign In</Link>
+                <Button className={assignStyle("/login")}>
+                    <Link to="/login">Sign In</Link>
                 </Button>
-                <Button className={style} onClick={changeStyle}>
-                    <Link to="/signup" style={{ textDecoration: 'none' }}>Sign Up</Link>
+                <Button className={assignStyle("/signup")}>
+                    <Link to="/signup">Sign Up</Link>
                 </Button>
             </div>
         </div>
