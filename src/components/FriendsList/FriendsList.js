540ef7fb3c0088815aec8f807fae2c0e4a6b08ab
Melly5 <92356658+Melly5@users.noreply.github.com>
2022-07-14 17:39:00 +0700

    Update FriendsList.js

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -1,45 +1,50 @@
 import React from "react";
+import { Box, ListItem, List, ListItemText, Grid } from "@mui/material";
 import "./FriendsList.css";
 
 export const FriendsList = () => {
+  const users = [
+    {
+      letters: "AЗ",
+      name: "Андрей Зурко",
+      isInNetwork: true,
+    },
+    {
+      letters: "ЕВ",
+      name: "Екатерина Владимирова",
+      isInNetwork: true,
+    },
+    {
+      letters: "AМ",
+      name: "Андрей Михайлов",
+      isInNetwork: false,
+    },
+    {
+      letters: "AМ",
+      name: "Марина Моор",
+      isInNetwork: false,
+    },
+  ];
+
   return (
-    <div className="friendslist">
-      <div className="container friend-list">
+    <Box className="friendslist">
+      <Box className="container friend-list">
         <div className="friends-header">Друзья</div>
         <div className="friends-items">
-          <div className="friends_block">
-            <div className="friends_round"></div>
-            <div className="friends_descr">
-              <div className="friends_name">Андрей Зурко</div>
-              <div class="friends_message">Написать сообщение</div>
+          {users.map(user => (
+            <div className="friends_block">
+              <div className="friends_round"></div>
+              <div className="friends_descr">
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                {user.isInNetwork && <Box className="status-icon"></Box>}
+                <span className="friends_name">{user.name}</span>
+                </Box>
+                <div class="friends_message">Написать сообщение</div>
+              </div>
             </div>
-          </div>
-
-          <div className="friends_block">
-            <div className="friends_round"></div>
-            <div className="friends_descr">
-              <div className="friends_name">Екатерина Владимирова</div>
-              <div class="friends_message">Написать сообщение</div>
-            </div>
-          </div>
-
-          <div className="friends_block">
-            <div className="friends_round"></div>
-            <div className="friends_descr">
-              <div className="friends_name">Андрей Михайлов</div>
-              <div class="friends_message">Написать сообщение</div>
-            </div>
-          </div>
-
-          <div className="friends_block">
-            <div className="friends_round"></div>
-            <div className="friends_descr">
-              <div className="friends_name">Александр Смирнов</div>
-              <div class="friends_message">Написать сообщение</div>
-            </div>
-          </div>
+          ))}
         </div>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
